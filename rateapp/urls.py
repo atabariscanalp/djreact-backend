3a1d4395720d86798cdac8d82e3f5ae6f3aeaeda
ata barış canalp <46068510+atabariscanalp@users.noreply.github.com>
2020-11-22 16:56:13 +0300

    Update urls.py
    -commented out static(settings.MEDIA...)

--- rateapp/urls.py
+++ rateapp/urls.py
@@ -45,5 +45,5 @@ urlpatterns = [
     path('auth/password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+ #   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
