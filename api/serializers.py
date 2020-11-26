e313fb8b2b7018e3ca156af4d1a7cd76acfc0106
ata barış canalp <46068510+atabariscanalp@users.noreply.github.com>
2020-11-26 15:36:28 +0300

    Update serializers.py
    -typo

--- api/serializers.py
+++ api/serializers.py
@@ -122,7 +122,7 @@ class UserSerializer(serializers.ModelSerializer):
     def get_profile_photo(self, obj):
         return obj.profile.profile_photo.url if obj.profile.profile_photo else ""
     
-class UserEditSerializer(serializer.ModelSerializer):
+class UserEditSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('username', 'first_name', 'last_name')
