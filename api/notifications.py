3f3058216318c3606d6298915862b7b1eda034ef
ata barış canalp <46068510+atabariscanalp@users.noreply.github.com>
2020-12-27 22:58:55 +0300

    -updated send_notifications function

--- api/notifications.py
+++ api/notifications.py
@@ -1,10 +1,17 @@
 from fcm_django.models import FCMDevice
 
-def send_notification(user_id,title, message, data):
+def send_notification(user_id, title, message, data):
    try:
       device = FCMDevice.objects.filter(user=user_id).first()
-      result = device.send_message(title=title,body=message,
-                          data=data,sound=True)
-      return result
+      kwargs = {
+        "content_available": True,
+        'extra_kwargs': {"priority": "high", "mutable_content": True, 'notification': data },
+      }
+      if device.type == 'ios':
+         device.send_message(title=title,body=message,data=data,sound='default',**kwargs)
+      else:
+         device.send_message(title=title,body=message,data=data,sound='default')
+      #result = device.send_message(title=title,body=message,data=data,sound=)
+      #return result
    except:
       pass
