7337d8b65ac70c8182c73837af50dbb1b5b6d1fe
ata barış canalp <46068510+atabariscanalp@users.noreply.github.com>
2020-12-29 16:18:23 +0300

    -updated notifications.py

--- api/notifications.py
+++ api/notifications.py
@@ -1,17 +1,19 @@
 from fcm_django.models import FCMDevice
+from users.models import CustomUser
 
 def send_notification(user_id, title, message, data):
    try:
-      device = FCMDevice.objects.filter(user=user_id).first()
-      kwargs = {
-        "content_available": True,
-        'extra_kwargs': {"priority": "high", "mutable_content": True, 'notification': data },
-      }
-      if device.type == 'ios':
-         device.send_message(title=title,body=message,data=data,sound='default',**kwargs)
-      else:
-         device.send_message(title=title,body=message,data=data,sound='default')
-      #result = device.send_message(title=title,body=message,data=data,sound=)
-      #return result
+      user = CustomUser.objects.get(pk=user_id)
+      device = FCMDevice.objects.get(user=user)
+#       kwargs = {
+#         "content_available": True,
+#         'extra_kwargs': {"priority": "high", "mutable_content": True, 'notification': data },
+#       }
+#       if device.type == 'ios':
+#          device.send_message(title=title,body=message,data=data,sound='default',**kwargs)
+#       else:
+#          device.send_message(title=title,body=message,data=data,sound='default')
+      result = device.send_message(title=title,body=message,data=data,sound='default')
+      return result
    except:
       pass
