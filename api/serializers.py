62794cc6a3dc5985332e14c5ff0e4926e6ce3e25
atabariscanalp <atabariscanalp@outlook.com>
2021-02-22 21:24:11 +0300

    -line 532

--- api/serializers.py
+++ api/serializers.py
@@ -530,7 +530,7 @@ class BlockedUsersSerializer(serializers.ModelSerializer):
             blocks.append(q.blocked_user.id)
         data = UserEditSerializer(blocks, many=True, read_only=True).data 
         dict = {v["username"]: v for v in data}
-        return dict
+        return blocks
 
 class AddBlockedUserSerializer(serializers.ModelSerializer):
     class Meta:
